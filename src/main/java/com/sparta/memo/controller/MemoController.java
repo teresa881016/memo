1738625a17c1dd1bfe410d673bc39d32da50a737
teresa881016 <teresa881016@gmail.com>
2023-08-29 11:43:26 +0900

    강한결합을 느슨한 결합으로

--- src/main/java/com/sparta/memo/controller/MemoController.java
+++ src/main/java/com/sparta/memo/controller/MemoController.java
@@ -3,7 +3,6 @@ package com.sparta.memo.controller;
 import com.sparta.memo.dto.MemoRequestDto;
 import com.sparta.memo.dto.MemoResponseDto;
 import com.sparta.memo.service.MemoService;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,10 +10,11 @@ import java.util.List;
 @RestController
 @RequestMapping("/api")
 public class MemoController {
+    // MemoController -> MemoService -> MemoRepository = 강한 결합
 
     private final MemoService memoService;
-    public MemoController(JdbcTemplate jdbcTemplate) {
-        this.memoService = new MemoService(jdbcTemplate);
+    public MemoController(MemoService memoService) {
+        this.memoService = memoService;
     }
 
     @PostMapping("/memos")
