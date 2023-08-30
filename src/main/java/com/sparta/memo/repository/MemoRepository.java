e47283300ea4225915f48769af1fd4757b3d6195
teresa881016 <teresa881016@gmail.com>
2023-08-30 15:19:16 +0900

    Query Method란 무엇일까

--- src/main/java/com/sparta/memo/repository/MemoRepository.java
+++ src/main/java/com/sparta/memo/repository/MemoRepository.java
@@ -3,9 +3,13 @@ package com.sparta.memo.repository;
 import com.sparta.memo.entity.Memo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 //@Component// bean 객체로 등록하기
 
 public interface MemoRepository extends JpaRepository<Memo, Long> { //DB 작업
+
+    List<Memo> findAllByOrderByModifiedAtDesc();
 
 
 
